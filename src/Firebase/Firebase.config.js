265a00767e9ffc565ee821460ae8a63fc6ed841a
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2022-07-27 22:11:47 +0600

    client side code done

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCII2dPxPkx7ZOUESxSGwhnxS8iRp7fzK8",
+  authDomain: "travel-be949.firebaseapp.com",
+  projectId: "travel-be949",
+  storageBucket: "travel-be949.appspot.com",
+  messagingSenderId: "359464342267",
+  appId: "1:359464342267:web:710a93c957b14b1b52965e"
+};
+export default firebaseConfig;
